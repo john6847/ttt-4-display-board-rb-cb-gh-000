0c356ea9e2ce70a3787b00c1395b56eebdbca549
John Bien-aime <20131256@ce.pucmm.edu.do>
2019-05-24 19:27:30 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,47 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+def display_board(board)
+
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+  #
+  #
+  # puts " X | X | X "
+  # puts "-----------"
+  # puts " X | X | X "
+  # puts "-----------"
+  # puts " X | X | X "
+  #
+  # puts " O | O | O "
+  # puts "-----------"
+  # puts " O | O | O "
+  # puts "-----------"
+  # puts " O | O | O "
+
+  # puts "   |   |   "
+  # puts "-----------"
+  # puts "   | X |   "
+  # puts "-----------"
+  # puts "   |   |   "
+  #
+  # puts " O |   |   "
+  # puts "-----------"
+  # puts "   |   |   "
+  # puts "-----------"
+  # puts "   |   |   "
+  #
+  # puts " O |   |   "
+  # puts "-----------"
+  # puts "   | X |   "
+  # puts "-----------"
+  # puts "   |   |   "
+  #
+  # puts " X | X | X "
+  # puts "-----------"
+  # puts "   |   |   "
+  # puts "-----------"
+  # puts "   |   |   "
+end
