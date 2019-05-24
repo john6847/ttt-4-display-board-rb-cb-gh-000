3241a577f903f8cbd765c4aa395d49fd4e9f1f48
John Bien-aime <20131256@ce.pucmm.edu.do>
2019-05-24 19:31:22 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -8,6 +8,13 @@ def display_board(board)
   puts "-----------"
   puts "   |   |   "
 
+puts "  |   |   "
+puts "-----------"
+puts "  | X |   "
+puts "-----------"
+puts "  |   |   "
+
+
   puts "   |   |   "
   puts "-----------"
   puts "   | X |   "
